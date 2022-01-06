3e5c8e2fb4cade2b0bb1f1c908f78ec65ceae704
Alwin Nedrum <nedrum@gmail.com>
2022-01-06 11:01:30 +0100

    Added simple posts test and layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<div class="wrapper">
+	<slot />
+</div>
+
+<style>
+	.wrapper {
+		margin: 0 auto;
+		width: 60vw;
+	}
+	:global(.more-link) {
+		display: none;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	//testing post
+	export const load = async ({ fetch }) => {
+		const res = await fetch('http://127.0.0.1/wordpress/wp-json/wp/v2/posts');
+		const posts = await res.json();
+		return {
+			props: {
+				posts
+			}
+		};
+	};
+</script>
+
+<script>
+	export let posts;
+</script>
+
+<h1>Blog</h1>
+{#each posts as post}
+	<h2>{post.title.rendered}</h2>
+	<p>{@html post.excerpt.rendered}</p>
+{/each}
+
+<!-- {JSON.stringify(posts)} -->
